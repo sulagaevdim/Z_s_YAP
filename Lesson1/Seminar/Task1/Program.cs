35baf04762660df517543311deecf887d94fa2da
sulagaev_dmitry <sulagaevdim@mail.ru>
2024-02-07 19:40:41 +0400

    add Task1

---
+++ Lesson1/Seminar/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
