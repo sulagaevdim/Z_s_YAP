2f961cf50d7824a989958f4ff1bd26c45927cbd9
sulagaev_dmitry <sulagaevdim@mail.ru>
2024-02-07 20:22:01 +0400

    add Task2

---
+++ Lesson1/Seminar/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// на вход принимает число N и выводит все числа от -N до N
+
+
+//Вариант1
+/*System.Console.Write("Ведите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+
+while (count <= N)
+{
+    Console.Write(count + " ");
+    count = count + 1;
+}*/
+
+
+//Вариант2
+System.Console.Write("Ведите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = -N; i <= N; i++)
+{
+    Console.Write(i + " ");
+}
