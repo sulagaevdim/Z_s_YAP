b90590a69d1290019757b770a713d0a14e36e0bd
sulagaev_dmitry <sulagaevdim@mail.ru>
2024-02-10 15:55:32 +0400

    add Task3

---
+++ Lesson2_Arrays/Lection/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// int N = 10;
+int[] array = {2, 4, 7, 5, 8, 1, 9, 4, 7, 5};
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i++;
+}
