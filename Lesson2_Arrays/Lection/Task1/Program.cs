1be7e4c7663785aa9f0e94b21061fc160c5c227f
sulagaev_dmitry <sulagaevdim@mail.ru>
2024-02-10 15:13:31 +0400

    add Task1

---
+++ Lesson2_Arrays/Lection/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[5];
+array[0] = 4;
+array[1] = 3;
+array[2] = 7;
+array[3] = 2;
+array[4] = 8;
+System.Console.WriteLine(array[0]);
+System.Console.WriteLine(array[1]);
+System.Console.WriteLine(array[2]);
+System.Console.WriteLine(array[3]);
+System.Console.WriteLine(array[4]);
+
+System.Console.WriteLine(array[5]);
+
+
+int[] array2 = new int[5] { 5, 6, 7, 8, 9 };
+
